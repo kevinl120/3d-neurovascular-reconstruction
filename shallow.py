1426bb473d7d639f2553054433c938a90b6ae39d
Kevin Li <kevinli120@yahoo.com>
2020-06-04 16:12:13 -0700

    Add Convolutional transpose and weighted loss, increase model size

--- shallow.py
+++ shallow.py
@@ -1,7 +1,7 @@
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras import backend as K
-from tensorflow.keras.layers import Input, Dense, Reshape, Flatten, Conv2D, MaxPooling2D, UpSampling3D, Conv3D, Conv3DTranspose
+from tensorflow.keras.layers import Input, Dense, Reshape, Flatten, Conv2D, MaxPooling2D, UpSampling3D, Conv3D, Conv3DTranspose, Dropout
 from tensorflow.keras.losses import binary_crossentropy
 from tensorflow.keras.models import Model
 
@@ -9,8 +9,23 @@ from constants import *
 import preprocess
 
 
+arr = np.zeros((64,64,64))
+for i in range(64):
+    for j in range(64):
+        for k in range(64):
+            arr[i][j][k] = i+j+k
+
+res = arr
+res = np.minimum(res, np.flip(arr, axis=0))
+res = np.minimum(res, np.flip(arr, axis=1))
+res = np.minimum(res, np.flip(arr, axis=2))
+res = np.minimum(res, np.flip(arr, axis=(0,1)))
+res = np.minimum(res, np.flip(arr, axis=(0,2)))
+res = np.minimum(res, np.flip(arr, axis=(1,2)))
+
 def weighted_bce(y_true, y_pred):
-    weights = (y_true * 40) + 1.
+    weights = (y_true * 2) + 1.
+    weights *= res
     bce = K.binary_crossentropy(y_true, y_pred)
     weighted_bce = K.mean(bce * weights)
     return weighted_bce
@@ -27,32 +42,44 @@ def make_dnn(**kwargs):
     x = inputs
 
     # Encoder
-    x = Conv2D(16, 7, activation='relu', padding='same')(x)
+    x = Conv2D(32, 7, activation='relu', padding='same')(x)
     x = MaxPooling2D(pool_size=(2, 2))(x)
-    x = Conv2D(32, 3, activation='relu', padding='same')(x)
-    x = MaxPooling2D(pool_size=(2, 2))(x)
-    x = Conv2D(32, 3, activation='relu', padding='same')(x)
-    x = MaxPooling2D(pool_size=(2, 2))(x)
+    x = Dropout(0.2)(x)
     x = Conv2D(64, 3, activation='relu', padding='same')(x)
     x = MaxPooling2D(pool_size=(2, 2))(x)
+    x = Dropout(0.2)(x)
     x = Conv2D(64, 3, activation='relu', padding='same')(x)
     x = MaxPooling2D(pool_size=(2, 2))(x)
+    x = Dropout(0.2)(x)
     x = Conv2D(128, 3, activation='relu', padding='same')(x)
     x = MaxPooling2D(pool_size=(2, 2))(x)
+    x = Dropout(0.2)(x)
+    x = Conv2D(128, 3, activation='relu', padding='same')(x)
+    x = MaxPooling2D(pool_size=(2, 2))(x)
+    x = Dropout(0.2)(x)
+    x = Conv2D(256, 3, activation='relu', padding='same')(x)
+    x = MaxPooling2D(pool_size=(2, 2))(x)
+    x = Dropout(0.2)(x)
     x = Flatten()(x)
 
     # FC
-    x = Dense(2048, activation='relu')(x)
-    x = Reshape((8, 8, 8, 4))(x)
+    x = Dense(4320, activation='relu')(x)
+    x = Dropout(0.5)(x)
+    x = Reshape((6, 6, 6, 20))(x)
 
     # Decoder
+    x = Dropout(0.2)(x)
     x = UpSampling3D(size=(2, 2, 2))(x)
-    x = Conv3D(128, 3, activation='relu', padding='same')(x)
+    x = Dropout(0.2)(x)
+    x = Conv3DTranspose(256, 3, activation='relu', padding='valid')(x)
+    x = Dropout(0.2)(x)
     x = UpSampling3D(size=(2, 2, 2))(x)
-    x = Conv3D(64, 3, activation='relu', padding='same')(x)
+    x = Conv3DTranspose(128, 3, activation='relu', padding='valid')(x)
+    x = Dropout(0.2)(x)
     x = UpSampling3D(size=(2, 2, 2))(x)
-    x = Conv3D(32, 3, activation='relu', padding='same')(x)
-    x = Conv3D(1, 3, activation='sigmoid', padding='same')(x)
+    x = Conv3DTranspose(64, 3, activation='relu', padding='valid')(x)
+    x = Dropout(0.2)(x)
+    x = Conv3DTranspose(1, 3, activation='sigmoid', padding='valid')(x)
     
     outputs = Reshape((64, 64, 64))(x)
 
@@ -61,13 +88,12 @@ def make_dnn(**kwargs):
 
 
 def main():
-    x_train, y_train = preprocess.load_data()
-    print(np.sum(x_train[0]))
-    x_train, y_train = unison_shuffled_copies(x_train, y_train)
+    # x_train, y_train = preprocess.load_data()
+    # x_train, y_train = unison_shuffled_copies(x_train, y_train)
     dnn = make_dnn()
-    dnn.compile(optimizer='Adam', loss=weighted_bce)
+    # dnn.compile(optimizer='Adam', loss=weighted_bce)
     dnn.summary()
-    history = dnn.fit(x_train, y_train, batch_size=None, epochs=1, validation_split=0.1)
+    # history = dnn.fit(x_train, y_train, batch_size=None, epochs=1, validation_split=0.1)
 
 if __name__ == '__main__':
     main()
