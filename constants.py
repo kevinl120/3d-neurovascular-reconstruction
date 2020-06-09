1425070b1cf2da7c6436f3c1f703a252d615c213
Kevin Li <kevinli120@yahoo.com>
2020-06-09 13:18:55 -0700

    Update constants.py

--- constants.py
+++ constants.py
@@ -6,7 +6,14 @@ PROJ_DIR = os.path.join(DATA_DIR, 'projection')
 VOXEL_TXT_DIR = os.path.join(DATA_DIR, 'voxel_txt')
 VOXEL_NPY_DIR = os.path.join(DATA_DIR, 'voxel_npy')
 
-PROJ_SHAPE = (256, 256)
+PROJ_LEN = 256
+PROJ_SHAPE = (PROJ_LEN, PROJ_LEN)
 CAMERA_DIRS = [(1,0,0), (-1,0,0), (0,1,0), (0,-1,0), (0,0,1), (0,0,-1)]
 NUM_VIEWS = len(CAMERA_DIRS)
-VOXEL_SHAPE = (64, 64, 64)
+VOXEL_LEN = 64
+VOXEL_SHAPE = (VOXEL_LEN, VOXEL_LEN, VOXEL_LEN)
+
+def unison_shuffled_copies(a, b):
+    assert len(a) == len(b)
+    p = np.random.permutation(len(a))
+    return a[p], b[p]
